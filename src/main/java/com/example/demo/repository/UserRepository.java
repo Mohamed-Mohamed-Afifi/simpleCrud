60d6e96add175212c1fe6838ef6389823312035b
Mohamed Samy <mohamedsamy2072000@gmail.com>
2022-10-24 03:10:21 +0200

    Added a method to find users by email

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Users;
+import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<Users, Integer> {
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("Select u From User u Where u.email = ?1")
+    Optional<User> findUsersByEmail(String email);
 }
