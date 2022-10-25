93b60d80feb64c3de8e01191885b26cfb50b6664
Mohamed Samy <mohamedsamy2072000@gmail.com>
2022-10-25 18:32:49 +0200

    Changed constraint on phone

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -4,6 +4,6 @@ create table core.users(
     uname VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL unique,
     age INT,
-    phone VARCHAR(15),
+    phone VARCHAR(11),
     PRIMARY KEY ( id )
 );
